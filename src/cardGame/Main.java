c5af3ad341447a94058a1d09b49847fc8c776661
bowstringhero <hyeonjun217.lee@gmail.com>
2023-07-31 12:28:44 +0900

    Algorithm Test

--- src/cardGame/Main.java
+++ src/cardGame/Main.java
@@ -6,6 +6,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int cardArr[][] = new int[n][m];
+        int max = 0;
 
+        for (int i = 0; i < n; i++) {
+            int rowMin = 10001;
+
+            for (int j = 0; j < m; j++) {
+                cardArr[i][j] = scanner.nextInt();
+
+                if (cardArr[i][j] < rowMin) {
+                    rowMin = cardArr[i][j];
+                }
+            }
+            if (max < rowMin) {
+                max = rowMin;
+            }
+        }
+        System.out.println("result = " + max);
     }
 }
